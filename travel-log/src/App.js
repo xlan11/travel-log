1ff0580bb2bd2a17028f65c643bb4ca6a3c2be10
xlan11 <alankelly@gmail.com>
2022-08-03 19:59:10 +0100

    initial commit

---
+++ travel-log/src/App.js
@@ -0,0 +1,25 @@
+import React from "react"
+import './App.css';
+import Header from "./components/Header"
+import Card from "./components/Card"
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Card />
+    </div>
+  )
+}
+
+// Photos from Unsplash
+// (can use these URLs as your img src)
+
+// Japan
+// https://source.unsplash.com/WLxQvbMyfas
+
+// Australia
+// https://source.unsplash.com/JmuyB_LibRo
+
+// Norway
+// https://source.unsplash.com/3PeSjpLVtLg
---
+++ travel-log/src/components/Card.js
@@ -0,0 +1,31 @@
+import React from "react"
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faLocationDot} from '@fortawesome/free-solid-svg-icons'
+
+// library.add(faLocationDot)
+
+function Card() {
+    return (
+      <main>
+        <div className="card">
+            <img src="https://source.unsplash.com/WLxQvbMyfas" alt="japan" className="card--img"/>    
+          <div className="card--details">
+            <div className="card--locations">
+              {/* <FontAwesomeIcon icon={faLocationDot} className="card--pin-icon" /> */}
+              <p className="card--country">Japan</p>
+              <a href="https://www.google.com" className="card--googlemaps">View on Google Maps</a>
+            </div>
+            <h2 className="card--location">Mount Fuji</h2>
+            <div className="card--description">
+                <p className ="card--paragraph bold">12 Jan, 2021 - 24 Jan, 2021</p>
+                <p className ="card--paragraph">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+            </div>
+          </div>       
+        </div>
+      </main>
+
+      )
+    }
+
+    export default Card
